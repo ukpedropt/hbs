6813f5f6813e718c8aed3ce863fff3986729bd38
Pedro Rodrigues <ukpedropt@hotmail.com>
2023-06-08 01:00:03 +0100

    Success page added

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
 
 @app.route('/')
 def index():
@@ -14,13 +23,23 @@ def register():
         email = request.form['email']
         password = request.form['password']
         
-        # Process the form data (e.g., store in database, etc.)
-        # Add your code here
+        # Create a new User instance
+        user = User(username=username, email=email, password=password)
+        
+        # Add the user to the database
+        db.session.add(user)
+        db.session.commit()
         
         # Redirect to a success page or another route
         return redirect(url_for('success'))
     
     return render_template('register.html')
 
+@app.route('/success')
+def success():
+    return 'Registration successful!'
+
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()  # Create the tables
     app.run(debug=True)
